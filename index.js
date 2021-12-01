1ce042c1887a5a3ff5b41d45061be70444ee7fd5
Cstylefly <52144668+Cstylefly@users.noreply.github.com>
2021-12-01 10:24:05 +0800

    修改

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = require('./dist/cstyle.min.js')
+    module.exports = require('./lib/cstyle.min.js')
 }else if(process.env.NODE_ENV == 'development'){
-    module.exports = require('./dist/cstyle.js')
+    module.exports = require('./lib/cstyle.js')
 }   
